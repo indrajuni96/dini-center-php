9827109f5e07d325b98a9786ce4674be9594d6a0
Indrajuni96 <indrajuniyanto96@gmail.com>
2021-07-30 13:02:53 +0700

    validation delete

--- admin/delete-gejala.php
+++ admin/delete-gejala.php
@@ -1,9 +1,26 @@
 <?php
 $get_id = $_GET['id'];
 
-$database->getReference("gejala/{$get_id}")->remove();
+$checkPengetahuanGejala = 'false';
 
-$_SESSION['message']  = "Data Berhasil Di Hapus";
-$_SESSION['msg_type'] = "success";
+$dataGejala = $database->getReference("gejala")->getValue();
+$dataPengetahuan = $database->getReference("pengetahuan")->getValue();
+// $dataHasilDiagnosa = $database->getReference("hasilDiagnosa")->getValue();
+
+foreach ($dataPengetahuan as $keyPengetahuan => $valuePengetahuan) {
+  if ($valuePengetahuan['idGejala1'] == $get_id || $valuePengetahuan['idGejala2'] == $get_id) {
+    $checkPengetahuanGejala = 'true';
+  }
+}
+
+if ($checkPengetahuanGejala == 'true') {
+  $_SESSION['message']  = "Gejala Masih Di Gunakan";
+  $_SESSION['msg_type'] = "error";
+} else {
+  $database->getReference("gejala/{$get_id}")->remove();
+
+  $_SESSION['message']  = "Data Berhasil Di Hapus";
+  $_SESSION['msg_type'] = "success";
+}
 
 echo '<script>window.location="index.php?act=data-gejala";</script>';
--- admin/delete-penyakit.php
+++ admin/delete-penyakit.php
@@ -1,9 +1,33 @@
 <?php
 $get_id = $_GET['id'];
 
-$database->getReference("penyakit/{$get_id}")->remove();
+$checkHasilDiagnosaPenyakit = 'false';
+$checkPengetahuanPenyakit = 'false';
 
-$_SESSION['message']  = "Data Berhasil Di Hapus";
-$_SESSION['msg_type'] = "success";
+$dataPenyakit = $database->getReference("penyakit")->getValue();
+$dataPengetahuan = $database->getReference("pengetahuan")->getValue();
+$dataHasilDiagnosa = $database->getReference("hasilDiagnosa")->getValue();
+
+foreach ($dataPengetahuan as $keyPengetahuan => $valuePengetahuan) {
+  if ($valuePengetahuan['idPenyakit'] == $get_id) {
+    $checkPengetahuanPenyakit = 'true';
+  }
+}
+
+foreach ($dataHasilDiagnosa as $keyHasilDiagnosa => $valueHasilDiagnosa) {
+  if ($valueHasilDiagnosa[0]['idPenyakit'] == $get_id) {
+    $checkHasilDiagnosaPenyakit = 'true';
+  }
+}
+
+if ($checkPengetahuanPenyakit == 'true' || $checkHasilDiagnosaPenyakit == 'true') {
+  $_SESSION['message']  = "Penyakit Masih Di Gunakan";
+  $_SESSION['msg_type'] = "error";
+} else {
+  $database->getReference("penyakit/{$get_id}")->remove();
+
+  $_SESSION['message']  = "Data Berhasil Di Hapus";
+  $_SESSION['msg_type'] = "success";
+}
 
 echo '<script>window.location="index.php?act=data-penyakit";</script>';
